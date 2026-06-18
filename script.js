9e4d79e1a457c119a344f0b76da3c956238c7e70
LoboLoco69 <logan.bowling1109@gmail.com>
2026-06-18 09:08:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const board = document.getElementById("board");
 
+for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+        const square = document.createElement("div");
+
+        if ((row + col) % 2 === 0) {
+            square.classList.add("square-light");
+        } else {
+            square.classList.add("square-dark");
+        }
+
+        board.appendChild(square);
+    }
+}
